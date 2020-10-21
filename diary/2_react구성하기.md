35527bafd6973e4ae6227125a6a366a316abd3c9
hayoung0Lee <hayoung0.lee@gmail.com>
2020-10-21 09:02:37 +0900

    finish copying templates to new project

--- diary/2_react구성하기.md
+++ diary/2_react구성하기.md
@@ -3,3 +3,30 @@ https://medium.com/@_diana_lee/cra%EC%97%86%EC%9D%B4-%EB%A6%AC%EC%95%A1%ED%8A%B8
 
 
 내가 react-app 구성한다음 cli를 만들어서 패키지 다운로드 받으면될랑강?
+
+
+* process 객체(브라우저에서 window같은거) - https://sollife.tistory.com/39
+    * https://nodejs.org/api/process.html#process_process_chdir_directory
+    * 현재 작업 디렉토리를 바꿀수있다. 
+
+    * process.cwd() https://stackoverflow.com/questions/9874382/whats-the-difference-between-process-cwd-vs-dirname
+
+
+* path모듈 사용법 파악하기
+  - resolve, join 
+  - https://www.hanumoka.net/2018/11/08/node-20181108-node-path-join-vs-resolve/
+
+* var spawn = require('cross-spawn');
+A cross platform solution to node's spawn and spawnSync.
+https://backback.tistory.com/362
+```
+const args = [
+    'install'
+]
+const proc = spawn('npm', args, {stdio: 'inherit'});
+```
+이렇게하면 npm install이 실행된다
+
+* var fs = require('fs-extra');
+같음
+https://www.npmjs.com/package/fs-extra
--- global-cli/index.js
+++ global-cli/index.js
@@ -1,5 +1,6 @@
-var fs = require('fs');
+var fs = require('fs-extra');
 var path = require('path');
+var spawn = require('cross-spawn');
 
 const cliInit = (argv) => {
     const commands = argv.slice(2);
@@ -13,25 +14,87 @@ const cliInit = (argv) => {
     createApp(commands[0]);
 }
 
-const createApp = (name) => {
+const createApp = (appName) => {
     // fs의 sync 인것들 그냥 사용!!
-    if(fs.existsSync(name)){
-        console.log('The directory `' + name + '` already exists. Aborting.');
+    if(fs.existsSync(appName)){
+        console.log('The directory `' + appName + '` already exists. Aborting.');
         process.exit(1);
     }
 
-    const root = path.resolve(name);
+    const root = path.resolve(appName);
     fs.mkdirSync(root);
     const packageJson = {
-        name: name,
+        name: appName,
         version: '0.0.1',
         description: 'app from create-hayoug-app',
         private: true,
     };
 
     fs.writeFileSync(path.join(root, 'package.json'), JSON.stringify(packageJson));
-
     console.log("made pacakage.json!!");
+
+    process.chdir(root); // root안으로 이동
+    // TODO - fix here run not working
+    run(root, appName); 
+}
+
+const run = (root, appName) =>{
+    const args = [
+        'install'
+    ]
+    const proc = spawn('npm', args, {stdio: 'inherit'}); // process실행
+    proc.on('close', function(code) {
+        if(code != 0){
+            console.log("npm error!");
+        } else {
+            getHayoungTemplate(root, appName)
+        }
+    })
+}
+
+const getHayoungTemplate = (root, appName) => {
+    // root: where it is currently running
+    const rootPackage= JSON.parse(fs.readFileSync(path.join(root, 'package.json'), {encoding: 'utf-8'}))
+    
+    rootPackage.main = "index.js";
+
+    rootPackage.scripts = {};
+    rootPackage.scripts["start"] = "webpack serve --mode development --open --hot";
+    rootPackage.scripts["build"] = "webpack --mode production";
+
+    rootPackage.dependencies = {}
+    rootPackage.dependencies["react"] = "^16.14.0";
+    rootPackage.dependencies["react-dom"] = "^16.14.0";
+    
+    rootPackage.devDependencies = {}
+    rootPackage.devDependencies["babel-core"] = "^6.26.3";
+    rootPackage.devDependencies["babel-loader"] = "^7.1.5";
+    rootPackage.devDependencies["babel-preset-env"] = "^1.7.0";
+    rootPackage.devDependencies["babel-preset-react"] = "^6.24.1";
+    rootPackage.devDependencies["html-webpack-plugin"] = "^4.5.0";
+    rootPackage.devDependencies["webpack"] = "^5.1.3" ;
+    rootPackage.devDependencies["webpack-cli"] = "^4.1.0";
+    rootPackage.devDependencies["webpack-dev-server"] = "^3.11.0";
+
+    fs.writeFileSync(
+        path.join(root, 'package.json'),
+        JSON.stringify(rootPackage, null, 2)
+    );
+
+    const args = [
+        'install'
+    ]
+    const proc = spawn('npm', args, {stdio: 'inherit'}); // process실행
+    proc.on('close', function(code) {
+        if(code != 0){
+            console.log("npm error!");
+        } else {
+            console.log("done!!");
+        }
+    })
+
+    const targetFolder = path.join(__dirname, '..', './templates');
+    fs.copySync(targetFolder, path.join(root));
 }
 
 module.exports = { 
